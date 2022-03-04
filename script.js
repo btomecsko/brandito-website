1bdca34c8a914e413a8b5793f5fd2f948f591ac0
btomecsko <brandon.tomecsko92@gmail.com>
2022-03-04 11:37:22 -0600

    html and css changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h6 = document.createElement("h6");
-h6.textContent = "This content added by JavaScript";
+//const h6 = document.createElement("h6");
+//h6.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h6);
+//document.querySelector("body").appendChild(h6);
