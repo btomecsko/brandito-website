a61aeb359d4950ad397b595c58b780f39c8d69e4
Brandon T <98200133+btomecsko@users.noreply.github.com>
2023-05-01 19:40:52 -0500

    changing old site with a react implementation

---
+++ old_site/brandito.js
@@ -0,0 +1,8 @@
+let brandito = {
+    name: "Brandon Tomecsko",
+    code: ["JavaScript", "Ruby", "Java", "Python"],
+    framework: ["React", "Bootstrap", "Spring Boot", "Rails", "Flask"],
+    cloud: ["AWS", "SalesForce"],
+    devops: ["Git", "Jest"]
+}
+
