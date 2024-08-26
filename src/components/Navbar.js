9b5fccdcd0d5a4d7c9d94f275944ba3fbe029652
Brandon T <98200133+btomecsko@users.noreply.github.com>
2024-08-26 08:54:51 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
   return (
     <div className={scrollColor ? "header header-bg" : "header"}>
       <Link to="/">
-        <h1>Brandito</h1>
+        <h1>branditoDev</h1>
       </Link>
       <ul className={click ? "nav-menu active" : "nav-menu"}>
         <li>
