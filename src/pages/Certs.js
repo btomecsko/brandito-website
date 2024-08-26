78652d2064a86ea8f626f4ad0ac8705c4e9b8bc8
Brandon T <98200133+btomecsko@users.noreply.github.com>
2024-08-26 17:20:59 -0400

    updating content

--- src/pages/Certs.js
+++ src/pages/Certs.js
@@ -5,41 +5,46 @@ import SfAI from "../assets/sfAI.png";
 import MuleDev from "../assets/muleDev.png";
 
 const Certs = () => {
-  return (
-    <div className="page-container">
-      <h1 className="project-heading">Certifications</h1>
-      <div className="project-container">
-        <div className="project-card">
-          <img src={MuleDev} alt="Salesforce Certified MuleSoft Developer I" />
-          <h2 className="project-title">Salesforce Certified MuleSoft Developer I</h2>
-          <div className="pro-details">
-            <p>Description of the Salesforce Certified MuleSoft Developer I certification</p>
-          </div>
+    const verificationLink = "https://trailhead.salesforce.com/en/credentials/verification/";
+
+    return (
+        <div className="page-container">
+        <h1 className="project-heading">Certifications</h1>
+        <div className="verification-container"> 
+            <a href={verificationLink}  target="_blank" rel="noopener noreferrer" className="btn btn-light">Verify Salesforce Certifications</a>
         </div>
-        <div className="project-card">
-          <img src={MuleAssoc} alt="Salesforce Certified MuleSoft Associate" />
-          <h2 className="project-title">Salesforce Certified MuleSoft Associate</h2>
-          <div className="pro-details">
-            <p>Description of the Salesforce Certified MuleSoft Associate certification</p>
-          </div>
+        <div className="project-container">
+            <div className="project-card">
+            <img src={MuleDev} alt="Salesforce Certified MuleSoft Developer I" />
+            <h4 className="project-title">Salesforce Certified MuleSoft Developer I</h4>
+            <div className="pro-details">
+                <p>Description of the Salesforce Certified MuleSoft Developer I certification</p>
+            </div>
+            </div>
+            <div className="project-card">
+            <img src={MuleAssoc} alt="Salesforce Certified MuleSoft Associate" />
+            <h4 className="project-title">Salesforce Certified MuleSoft Associate</h4>
+            <div className="pro-details">
+                <p>Description of the Salesforce Certified MuleSoft Associate certification</p>
+            </div>
+            </div>
+            <div className="project-card">
+            <img src={SfAI} alt="Salesforce Certified AI Associate" />
+            <h4 className="project-title">Salesforce Certified AI Associate</h4>
+            <div className="pro-details">
+                <p>Description of the Salesforce Certified AI Associate certification</p>
+            </div>
+            </div>
+            <div className="project-card">
+            <img src={SfAssoc} alt="Salesforce Certified Associate" />
+            <h4 className="project-title">Salesforce Certified Associate</h4>
+            <div className="pro-details">
+                <p>Description of the Salesforce Certified Associate certification</p>
+            </div>
+            </div>
         </div>
-        <div className="project-card">
-          <img src={SfAI} alt="Salesforce Certified AI Associate" />
-          <h2 className="project-title">Salesforce Certified AI Associate</h2>
-          <div className="pro-details">
-            <p>Description of the Salesforce Certified AI Associate certification</p>
-          </div>
         </div>
-        <div className="project-card">
-          <img src={SfAssoc} alt="Salesforce Certified Associate" />
-          <h2 className="project-title">Salesforce Certified Associate</h2>
-          <div className="pro-details">
-            <p>Description of the Salesforce Certified Associate certification</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Certs;
